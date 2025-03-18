6fd0f56fb875d1a615cfd1610232551f63db4ebe
Julia Menegaz Bueno <juliamenegazb568@gmail.com>
2025-03-17 21:38:17 -0300

    17/03/2025

--- exercicio6/AT8.java
+++ exercicio6/AT8.java
@@ -8,7 +8,14 @@ public class AT8 {
         ArrayList<String> paises = new ArrayList<>();
         
         paises.add("Brasil");
-        paises.add("")
+        paises.add("Russia");
+       paises.add("Alemanha");
+       paises.add("ira");
+       paises.add("Polonia");
+       
+       for(int i = 0; i < paises.size(); i ++) {
+        System.out.println("Pais "+ (i + 1) + ": " + paises.get(i));
+    }
     }
 
 }
