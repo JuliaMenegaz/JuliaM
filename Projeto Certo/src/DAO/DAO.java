16cd81556800d2f4fdcb93be611f3edc030f4ed9
Julia Menegaz Bueno <juliamenegazb568@gmail.com>
2025-07-08 21:57:30 -0300

    08/07/2025

---
+++ Projeto Certo/src/DAO/DAO.java
@@ -0,0 +1,11 @@
+package DAO;
+
+import Model.Carro;
+
+public class DAO {
+
+    public void alterarCarro(Carro carroEmEdicao) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+}
