8a108a76515153b7296b46b80324be02547a91e6
Julia Menegaz Bueno <juliamenegazb568@gmail.com>
2025-07-18 19:22:08 -0300

    18/07/2025

---
+++ Projeto integrador3.sql
@@ -0,0 +1,26 @@
+create database ProjetoIntegrador;
+
+create table Carros(
+id int not null primary key auto_increment,
+marca varchar(40),
+modelo varchar(20),
+ano int,
+cor varchar(20),
+estado varchar(20),
+caracteristicas varchar(30),
+acessorios varchar(30),
+valor double
+);
+
+create table Usuario(
+id int not null primary key auto_increment,
+usuario varchar(40),
+senha varchar(20)
+);
+
+show tables;
+drop table Carros;
+
+select * from Carros; 
+
+select * from carros;
